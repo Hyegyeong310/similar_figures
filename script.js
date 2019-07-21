d11182eb6ca7f2d00431d32a468ceb98a1e9029c
Hyegyeong310 <hyegyeong310@gmail.com>
2019-07-22 01:05:03 +0900

    [Add] count face in image

---
+++ script.js
@@ -0,0 +1,19 @@
+const imageUpload = document.getElementById('jsImageUpload');
+const faceLength = document.getElementById('jsFaceLength');
+
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(start);
+
+function start() {
+  imageUpload.addEventListener('change', async () => {
+    const image = await faceapi.bufferToImage(imageUpload.files[0]);
+    const detections = await faceapi
+      .detectAllFaces(image)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+    faceLength.innerHTML = detections.length;
+  });
+}
