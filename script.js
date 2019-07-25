fcb9d7af0ce74de8e6aebbcaa7a0ff52ea018593
Hyegyeong310 <hyegyeong310@gmail.com>
2019-07-26 01:14:15 +0900

    [Modify] find face, and add similar animal label.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const imageUpload = document.getElementById('jsImageUpload');
+const loaded = document.getElementById('jsLoaded');
 const faceLength = document.getElementById('jsFaceLength');
 const imageView = document.getElementById('jsImageView');
 
@@ -8,7 +9,11 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start);
 
-function start() {
+async function start() {
+  const labeledFaceDescriptors = await loadLabeledImages();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.0003);
+  // loaded.innerHTML = '';
+  loaded.classList.add('hidden');
   imageUpload.addEventListener('change', async () => {
     const image = await faceapi.bufferToImage(imageUpload.files[0]);
     imageView.innerHTML = '';
@@ -21,12 +26,40 @@ function start() {
       .detectAllFaces(image)
       .withFaceLandmarks()
       .withFaceDescriptors();
-    faceLength.innerHTML = detections.length;
+    faceLength.innerHTML = `Face: ${detections.length}`;
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    resizedDetections.forEach(detection => {
-      const box = detection.detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, { label: 'Face' });
+    const results = resizedDetections.map(d => {
+      console.log(d);
+      return faceMatcher.findBestMatch(d.descriptor);
+    });
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result.toString()
+      });
       drawBox.draw(canvas);
     });
   });
+}
+
+function loadLabeledImages() {
+  const labels = ['Cat', 'Dog', 'Fox', 'Rabbit'];
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = [];
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(
+          `https://raw.githubusercontent.com/Hyegyeong310/similar_figures/master/labeled_images/${label}/${i}.jpg`
+        );
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        if (detections) {
+          descriptions.push(detections.descriptor);
+        }
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
 }
